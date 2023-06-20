6d43249bd77810c2de9945f0a0098c12ba09abf5
Damglador <vse.stopchanskyi@gmail.com>
2023-06-21 01:45:51 +0300

    Оновлення з шрифтом

--- Ukrainian_Language/Ukrainian_Language/Ukrainian_Language.cs
+++ Ukrainian_Language/Ukrainian_Language/Ukrainian_Language.cs
@@ -10,15 +10,32 @@ using System.Net;
 using System.Threading.Tasks;
 using static MeshCombineStudio.CombinedLODManager;
 using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Collections;
+using TMPro;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
 
 public class Ukrainian_Language : Mod
 {
     public static string dataFolder = HLib.path_modsFolder + "\\ModData\\Ukrainian Language\\";
     public static string lastLanguageFile = dataFolder + "\\" + "Last Language Used.txt";
-
-    public void Start()
+    AssetBundle asset;
+    public IEnumerator Start()
     {
-        Log("Модифікація успішно завантажена!");
+        Log("Ukrainian Language loaded!");
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(GetEmbeddedFileBytes("thefont.asset"));
+        yield return request;
+        asset = request.assetBundle;
+        Font CyrillicFont = asset.LoadAsset<Font>("ChineseRocks-Cyrillic");
+        if (CyrillicFont != null)
+        {
+            Log("Шрифт успішно завантажено?");
+        }
+        else
+        {
+            Debug.LogError("Не вдалось завантажити шрифт");
+        }
         if (!Directory.Exists(dataFolder))
         {
             try
@@ -33,39 +50,17 @@ public class Ukrainian_Language : Mod
         }
         if (!File.Exists(lastLanguageFile))
         {
-            Log("Файл пам'яті відсутній.");
+            Debug.LogWarning("Файл пам'яті відсутній.");
             try
             {
-                using (StreamWriter sw = new StreamWriter(lastLanguageFile))
-                {
-                }
+                using (StreamWriter sw = new StreamWriter(lastLanguageFile)) { }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 Debug.LogError("Виникла помилка при створенні файлу пам'яті");
             }
         }
-        string repositoryUrl = "https://github.com/Damglador/Raft-UA/raw/l10n_main/uk/Loc.csv";
-        string savePath = dataFolder + "\\Loc.csv"; // Замініть на шлях, де ви хочете зберегти файл
-
-        using (WebClient webClient = new WebClient())
-        {
-            try
-            {
-                webClient.Encoding = Encoding.UTF8; // Встановлюємо кодування UTF-8
-                string fileContent = webClient.DownloadString(repositoryUrl);
-
-                File.WriteAllText(savePath, fileContent, Encoding.UTF8);
-
-                Log("Файл перекладу успішно завантажено з репозиторію: " + repositoryUrl);
-            }
-            catch (Exception)
-            {
-                Debug.LogError("Виникла помилка при завантаженні файлу\nБудь-ласка перевірте з'єднання з інтернетом, щоб оновити чи завантажити файл перекладу, або зверніться до головного кодера. Контакти:\nDiscord: damglador\nSteam: Damglador, та й будь де впринципі Damglador");
-            }
-        }
-
-        ImportLocalizationFromCSV("Loc.csv");
+        UpdateTranslationFile();
         RestoreLanguage();
 
     }
@@ -82,6 +77,17 @@ public class Ukrainian_Language : Mod
     }
     public void Update()
     {
+        Font CyrillicFont = asset.LoadAsset<Font>("ChineseRocks-Cyrillic");
+        Text[] textComponents = FindObjectsOfType<Text>();
+
+        foreach (Text textComponent in textComponents)
+        {
+            if (textComponent.font != CyrillicFont)
+            {
+                textComponent.font = CyrillicFont;
+                Log("Шрифт компоненту «" + textComponent + "» було замінено на «" + CyrillicFont + "»");
+            }
+        }
         string content = File.ReadAllText(lastLanguageFile);
         if (content != LocalizationManager.CurrentLanguage)
         {
@@ -92,10 +98,44 @@ public class Ukrainian_Language : Mod
 
     public void OnModUnload()
     {
+        asset.Unload(true);
         Log("Ukrainian Language has been unloaded!");
     }
+    [ConsoleCommand(name: "UpdateTranslationFile", docs: "Download latest version of translation from GitHub repository")]
 
+    public static void UpdateTranslationFile()
+    {
+        string repositoryUrl = "https://github.com/Damglador/Raft-UA/raw/l10n_main/uk/Loc.csv";
+        string savePath = dataFolder + "\\Loc.csv"; // Замініть на шлях, де ви хочете зберегти файл
+
+        using (WebClient webClient = new WebClient())
+        {
+            try
+            {
+                webClient.Encoding = Encoding.UTF8; // Встановлюємо кодування UTF-8
+                string fileContent = webClient.DownloadString(repositoryUrl);
+
+                File.WriteAllText(savePath, fileContent, Encoding.UTF8);
+
+                Debug.Log("Файл перекладу успішно завантажено з репозиторію: " + repositoryUrl);
+            }
+            catch (WebException e)
+            {
+                string errorMessage = "Виникла помилка при завантаженні файлу перекладу з репозиторію GitHub\nБудь ласка перевірте з'єднання з інтернетом, щоб оновити чи завантажити файл перекладу, або зверніться до головного кодера.\nКонтакти:\nDiscord: damglador\nSteam: Damglador, та й будь-де впринципі Damglador";
+                if (e.Status == WebExceptionStatus.ConnectFailure || e.Status == WebExceptionStatus.Timeout)
+                {
+                    Debug.LogError(errorMessage);
+                }
+                else
+                {
+                    Debug.LogError(errorMessage);
+                }
+            }
+        }
+        ImportLocalizationFromCSV("Loc.csv");
+    }
     [ConsoleCommand(name: "ImportLocalizationFromCSV", docs: "File must be in ModData folder and seperator must be ';'.\nUsage: ImportLocalizationFromCSV filename")]
+
     public static void ImportLocalizationFromCSVCommand(string[] args)
     {
         if (args == null || args.Length == 0)
