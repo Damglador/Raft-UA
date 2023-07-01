788f72a15d32c540d205a38d0e302c50ca075e58
Damglador <vse.stopchanskyi@gmail.com>
2023-07-02 02:02:28 +0300

    О більше шрифтів, більше!

--- Ukrainian_Language/Ukrainian_Language/Ukrainian_Language.cs
+++ Ukrainian_Language/Ukrainian_Language/Ukrainian_Language.cs
@@ -16,15 +16,19 @@ namespace UkrainianLanguage
     public class Main : Mod
     {
         public static string dataFolder = HLib.path_modsFolder + "\\ModData\\Ukrainian Language\\";
-        public static string lastLanguageFile = dataFolder + "\\" + "Last Language Used.txt";
+        public static string lastLanguageFile = dataFolder + "\\Last Language Used.txt";
+        public static string translationFile = dataFolder + "\\Loc.csv";
         AssetBundle asset;
         Harmony harmony;
-        public static TMP_FontAsset TMP_TTMakrsRoughCyrillic;
-        public static TMP_FontAsset TMP_ChineseRocksCyrillic;
-        public static TMP_FontAsset TMP_ChineseRocksCyrillic_RML;
+        public static TMP_FontAsset TTMakrsRoughCyrillic_TMP;
+        public static TMP_FontAsset ChineseRocksCyrillic_TMP;
+        public static TMP_FontAsset ChineseRocksCyrillicUI_TMP;
         public static Font ChineseRocksCyrillic;
+        public static TMP_FontAsset CalibriBold_TMP;
+        public static TMP_FontAsset LiberationSansBoldCyrillic_TMP;
+        public static TMP_FontAsset UbuntuBoldCyrillic_TMP;
         public static Main Instance { get; set; }
-        public static string modVersion = "1.1.1";
+        public static string modVersion = "1.1.3";
         public static string modName = "Ukrainian Language";
         public static string logPrefix = $"[{modName}] : ";
 
@@ -32,29 +36,28 @@ namespace UkrainianLanguage
         {
             (harmony = new Harmony("com.damglador.UkrainianLanguage")).PatchAll();
             Instance = this;
+            //============================== Loading Font ====================================
             AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(GetEmbeddedFileBytes("thefont.asset"));
             yield return request;
             asset = request.assetBundle;
-            TMP_TTMakrsRoughCyrillic = asset.LoadAsset<TMP_FontAsset>("TTMarksRough-Bold SDF");
-            TMP_ChineseRocksCyrillic = asset.LoadAsset<TMP_FontAsset>("ChineseRocks-Cyrillic SDF");
-            TMP_ChineseRocksCyrillic_RML = asset.LoadAsset<TMP_FontAsset>("ChineseRocks-Cyrillic for RML SDF");
+            TTMakrsRoughCyrillic_TMP = asset.LoadAsset<TMP_FontAsset>("TTMarksRough-Bold SDF");
+            ChineseRocksCyrillic_TMP = asset.LoadAsset<TMP_FontAsset>("ChineseRocks-Cyrillic SDF");
+            ChineseRocksCyrillicUI_TMP = asset.LoadAsset<TMP_FontAsset>("ChineseRocks-Cyrillic for RML SDF");
             ChineseRocksCyrillic = asset.LoadAsset<Font>("ChineseRocks-Cyrillic");
-            if (ChineseRocksCyrillic == null)
-                Debug.LogError(logPrefix + "Не вдалось завантажити TTMakrsRoughCyrillic TMPFont");
+            CalibriBold_TMP = asset.LoadAsset<TMP_FontAsset>("Calibri-Bold SDF");
+            LiberationSansBoldCyrillic_TMP = asset.LoadAsset<TMP_FontAsset>("LiberationSans-Bold Cyrillic SDF");
+            UbuntuBoldCyrillic_TMP = asset.LoadAsset<TMP_FontAsset>("Ubuntu-Bold Cyrillic SDF");
+            doesThisTMPFontExist(TTMakrsRoughCyrillic_TMP);
+            doesThisTMPFontExist(ChineseRocksCyrillic_TMP);
+            doesThisTMPFontExist(ChineseRocksCyrillicUI_TMP);
+            doesThisTMPFontExist(CalibriBold_TMP);
+            doesThisTMPFontExist(LiberationSansBoldCyrillic_TMP);
+            doesThisTMPFontExist(UbuntuBoldCyrillic_TMP);
+            if (ChineseRocksCyrillic != null)
+                Log("ChineseRocksCyrillic успішно завантажено");
             else
-                Log("TTMakrsRoughCyrillic TMPFont успішно завантажено");
-            if (TMP_ChineseRocksCyrillic_RML == null)
-                Debug.LogError(logPrefix + "Не вдалось завантажити ChineseRocksCyrillic TMPFont for RML");
-            else
-                Log("ChineseRocksCyrillic TMPFont for RML успішно завантажено");
-            if (TMP_ChineseRocksCyrillic == null)
-                Debug.LogError(logPrefix + "Не вдалось завантажити ChineseRocksCyrillic TMPFont");
-            else
-                Log("ChineseRocksCyrillic TMPFont успішно завантажено");
-            if (ChineseRocksCyrillic == null)
-                Debug.LogError(logPrefix + "Не вдалось завантажити ChineseRocksCyrillic Font");
-            else
-                Log("ChineseRocksCyrillic Font успішно завантажено");
+                Debug.LogError(logPrefix + "Не вдалось завантажити ChineseRocksCyrillic");
+            //================================================================================
             if (!Directory.Exists(dataFolder)) 
             {
                 Debug.LogWarning(logPrefix + "Data folder cant be found");
@@ -67,13 +70,18 @@ namespace UkrainianLanguage
                 Patch_TMP_Awake.Postfix(t);
             Log($"Mod loaded! Version {modVersion}");
         }
-
+        public void doesThisTMPFontExist (TMP_FontAsset font)
+        {
+            if (font != null)
+                Debug.Log(logPrefix + $"{font.name} успішно завантажено");
+            else
+                Debug.LogError(logPrefix + $"Не вдалось завантажити {font.name}");
+        }
         public void StartUpdateAfterDelayRoutine()
         {
             StartCoroutine(UpdateAfterDelayRoutine());
         }
-
-        //Font loader ==================================================
+        //======================================= Font loader ==================================================
         public IEnumerator UpdateAfterDelayRoutine()
         {
             yield return new WaitForSeconds(0.1f);
@@ -87,11 +95,12 @@ namespace UkrainianLanguage
             }
             Log($"Updated font for {upd} text components");
         }
+        //=======================================================================================================
         public static bool UpdateFont(Text __instance)
         {
             if (__instance.font.name != "ChineseRocks")
                 return false;
-            __instance.font = ChineseRocksCyrillic; 
+            __instance.font = ChineseRocksCyrillic;
             return true;
         }
         public void RestoreLanguage()
@@ -132,14 +141,15 @@ namespace UkrainianLanguage
         public static void UpdateTranslationFile()
         {
             string repositoryUrl = "https://github.com/Damglador/Raft-UA/raw/l10n_main/uk/Loc.csv";
-            string savePath = dataFolder + "\\Loc.csv"; // Замініть на шлях, де ви хочете зберегти файл
             using (WebClient webClient = new WebClient())
             {
                 try
                 {
                     webClient.Encoding = Encoding.UTF8; // Встановлюємо кодування UTF-8
                     string fileContent = webClient.DownloadString(repositoryUrl);
-                    File.WriteAllText(savePath, fileContent, Encoding.UTF8);
+                    File.WriteAllText(translationFile, fileContent, Encoding.UTF8);
+                    LocalizationManager.Sources[0].Import_CSV(null, File.ReadAllText(translationFile, Encoding.UTF8), eSpreadsheetUpdateMode.Merge, ';');
+                    LocalizationManager.LocalizeAll(true);
                     Debug.Log(logPrefix + $"Файл перекладу успішно завантажено з репозиторію: {repositoryUrl}");
                 }
                 catch (WebException e)
@@ -155,52 +165,9 @@ namespace UkrainianLanguage
                     }
                 }
             }
-            ImportLocalizationFromCSV("Loc.csv");
-        }
-
-        [ConsoleCommand(name: "ImportLocalizationFromCSV", docs: "File must be in ModData folder and seperator must be ';'.\nUsage: ImportLocalizationFromCSV filename")]
-        public static void ImportLocalizationFromCSVCommand(string[] args)
-        {
-            if (args == null || args.Length == 0)
-            {
-                Debug.LogError("Invalid argument.");
-                Debug.LogError("Type 'help ImportLocalizationFromCSV'");
-                return;
-            }
-
-            if (!ImportLocalizationFromCSV(args[0]))
-                Debug.LogError("Type 'help ImportLocalizationFromCSV'");
-        }
-
-        public static bool ImportLocalizationFromCSV(string filename)
-        {
-            if (filename.IsNullOrEmpty())
-            {
-                Debug.LogError("Invalid filename.");
-                return false;
-            }
-
-            if (!File.Exists(dataFolder + filename))
-            {
-                Debug.LogError($"No file with name {filename} in ModData.");
-                return false;
-            }
-
-            Debug.Log(LocalizationManager.Sources[0].Import_CSV(null, File.ReadAllText(dataFolder + filename, Encoding.UTF8), eSpreadsheetUpdateMode.Merge, ';'));
-            LocalizationManager.LocalizeAll(true);
-            return true;
-        }
-
-        public static HNotification ErrorNotification(string message)
-        {
-            return FindObjectOfType<HNotify>().AddNotification(HNotify.NotificationType.normal, message, 10, HNotify.ErrorSprite);
-        }
-
-        public static HNotification SuccessNotification(string message)
-        {
-            return FindObjectOfType<HNotify>().AddNotification(HNotify.NotificationType.normal, message, 5, HNotify.CheckSprite);
         }
     }
+    //======================================= Font loader ==============================================================
     [HarmonyPatch(typeof(Text))]
     [HarmonyPatch("text", MethodType.Setter)]
     static class Patch_Text_Setter
@@ -218,17 +185,17 @@ namespace UkrainianLanguage
             Main.Instance.StartUpdateAfterDelayRoutine();
         }
     }
-    //Notebook TMPFont loader ==================================================
+    //======================================= Notebook TMPFont loader ==================================================
     [HarmonyPatch(typeof(LocalizeTarget_TextMeshPro_Label), "SetFont")]
     static class Patch_SetLabelFont
     {
         static void Prefix(ref TMP_FontAsset newFont)
         {
             if (LocalizationManager.CurrentLanguage == "Українська")
-                newFont = Main.TMP_TTMakrsRoughCyrillic;
+                newFont = Main.TTMakrsRoughCyrillic_TMP;
         }
     }
-    //TMPFont loader ==================================================
+    //=========================================== TMPFont loader ========================================================
     [HarmonyPatch]
     static class Patch_TMP_Awake
     {
@@ -236,18 +203,19 @@ namespace UkrainianLanguage
         public static void Postfix(TMP_Text __instance)
         {
             if (__instance.name.StartsWith("TextMeshPro", StringComparison.OrdinalIgnoreCase) && (__instance.font.name == "ChineseRocks SDF" || __instance.font.name == "ChineseRocks SDF UI"))
-            {
-                __instance.font = Main.TMP_ChineseRocksCyrillic;
-            } 
-            else if (__instance.font.name == "RML_BaseFont" || __instance.font.name == "ChineseRocks SDF" || __instance.font.name == "ChineseRocks SDF UI")
-            {
-                __instance.font = Main.TMP_ChineseRocksCyrillic_RML;
-            }
-            else if (__instance.font.name == "tt_marks_rough_bold")
-            {
-                __instance.font = Main.TMP_TTMakrsRoughCyrillic;
-            }
-
+                __instance.font = Main.ChineseRocksCyrillic_TMP;
+            else if (__instance.font.name == "RML_BaseFont" || __instance.font.name == "ChineseRocks SDF" || __instance.font.name == "ChineseRocks SDF UI" || __instance.font.name == "ServerName")
+                __instance.font = Main.ChineseRocksCyrillicUI_TMP;
+            else if (__instance.font.name == "tt_marks_rough_bold SDF")
+                __instance.font = Main.TTMakrsRoughCyrillic_TMP;
+            else if (__instance.font.name == "LiberationSans SDF")
+                __instance.font = Main.LiberationSansBoldCyrillic_TMP;
+            else if (__instance.font.name == "Ubuntu-Bold SDF")
+                __instance.font = Main.UbuntuBoldCyrillic_TMP;
+            /*string lastFont = __instance.font.name;
+            __instance.font = Main.CalibriBold_TMP;
+            if (lastFont != Main.CalibriBold_TMP.name && lastFont != "ChineseRocks SDF" && lastFont != "tt_marks_rough_bold")
+                Main.Logger.Log($"Changed font for {__instance.name}    old font: {lastFont}" );*/
         }
         [HarmonyPatch(typeof(TextMeshProUGUI), "Awake")]
         static void Postfix(TextMeshProUGUI __instance) => Postfix(__instance as TMP_Text);
@@ -257,7 +225,8 @@ namespace UkrainianLanguage
     {
         static void Postfix (Block __instance)
         {
-            if (__instance is Storage_Small) { Main.Instance.StartUpdateAfterDelayRoutine(); }
+            if (__instance is Storage_Small) 
+                Main.Instance.StartUpdateAfterDelayRoutine();
         }
     }
 }
